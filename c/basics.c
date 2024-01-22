b51238a2d8763fa21916badaa75a5e3aa1b692c0
Taquavioncandy <2027shuler.quan@fcboe.org>
2024-01-22 14:50:11 -0500

    message

---
+++ c/basics.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+	printf("this is my c basics file");  // write an output statment 
+       
+	int num;
+        printf("Enter a number:");
+          scanf("%i", &num);
+
+	  printf("Your number was %i\n", num);
+	return 0;
+       
+}	
